3417a2b2773988b24ef51198a6ca5450dc1b9286
Abuawad <746951@student.dusd.net>
2023-03-10 09:52:28 -0800

    2.3
    finished 2.3

--- Unit2BasicGameplay/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         if (transform.position.x < -xRange)
         {
